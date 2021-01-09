426e81b08a84e08a905ff74100ca2c663ba2f85b
venkateswaran <venkateswaran300@gmail.com>
2021-01-09 19:40:04 +0530

    controller

---
+++ src/main/java/com/venkat/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.venkat.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/getGreating")
+	public String greatings() {
+		return "GitHub action demo....";
+	}
+}
